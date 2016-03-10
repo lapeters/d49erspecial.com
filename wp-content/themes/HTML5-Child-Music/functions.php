756cf9a3f6e24efc384f918cc91dc8afae63e161
Lindsay <lpeters322@gmail.com>
2016-03-09 21:53:18 -0800

    Deregister Parent Theme

--- wp-content/themes/HTML5-Child-Music/functions.php
+++ wp-content/themes/HTML5-Child-Music/functions.php
@@ -17,3 +17,18 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 /*  Add your own functions below this line.
     ======================================== */ 
 
+    // De-register HTML5 Blank styles
+    function html5blank_styles_make_child_active()
+    {
+    wp_deregister_style('html5blank'); // Enqueue it!
+    }
+    add_action('wp_enqueue_scripts', 'html5blank_styles_make_child_active', 100); // Add Theme Child Stylesheet
+
+    // Load HTML5 Blank Child styles
+    function html5blank_styles_child()
+    {
+    wp_register_style('html5blank-child', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('html5blank-child'); // Enqueue it!
+    }
+    add_action('wp_enqueue_scripts', 'html5blank_styles_child'); // Add Theme Child Stylesheet
+    
